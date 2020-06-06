f663cb5a44248380d890d384bea285629231c025
hackagon <phonghiavan@gmail.com>
2020-06-06 10:21:33 +0700

    prevent cors

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ const app = express();
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  next();
+});
+
 app.use("/images", express.static("uploads"))
 
 app.use("/api", require("./routes/api"));
